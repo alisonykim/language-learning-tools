56b74a3f680b015bbfdeecddb33abec9955c5f96
alisonykim <alison.y.kim@berkeley.edu>
2023-06-06 16:12:19 +0200

    Edit docstrings

--- fi/vowel_harmony.py
+++ fi/vowel_harmony.py
@@ -2,7 +2,16 @@
 # -*- coding: utf-8 -*-
 # vowel_harmony.py
 
-"""Master Finnish vowel harmony."""
+"""
+Master Finnish vowel harmony.
+
+Rules for native non-compound Finnish words:
+	1. All vowels of ```word``` in "AOU" -> front
+	2. All vowels of ```word``` in "AOUIE" -> front + neutral
+	3. All vowels of ```word``` in "ÄÖY" -> back
+	4. All vowels of ```word``` in "ÄÖYIE" -> back + neutral
+	5. All vowels of ```word``` in "IE" -> neutral
+"""
 
 from typing import List
 
@@ -16,38 +25,46 @@ def return_vowel_group(word: str) -> str:
 	"""
 	Determine the vowel group that preserves vowel harmony for ```word```.
 	
-	Rules:
-		1. All vowels of ```word``` in "AOU" or "AOUIE" -> "AOU"
-		2. All vowels of ```word``` in "ÄÖY", "AÖYIE", or "IE" -> "ÄÖY"
-	
 	Note:
 		Assumes that ```word``` is NOT a compound and has native Finnish etymology: a native non-compound word cannot contain vowels from the group {a, o, u} *together* with vowels from the group {ä, ö, y}.
 	
 	Example:
-	>>> word1 = 'Oulu'
-	>>> word2 = 'Jyväskylää'
-	>>> word3 = 'Helsingi'
-	>>> word4 = 'Rovaniemi'
+	>>> word1 = 'Suomi' # "UOI" in "AOUIE"
+	>>> word2 = 'Sveitsi' # "EII" in "IE"
+	>>> word3 = 'Saksa' # "AA" in "AOU"
+	>>> word4 = 'Venäjä' # "EÄÄ" in "ÄÖYIE"
 	>>> return_vowel_group(word1)
-	'aou'
+	'front + neutral'
 	>>> return_vowel_group(word2)
-	'äöy'
+	'neutral'
 	>>> return_vowel_group(word3)
-	'äöy'
+	'front'
 	>>> return_vowel_group(word4)
-	'aou'
+	'back + neutral'
 	"""
 	vowels = get_vowels(word)
-	if any(vowel in 'äöy' for vowel in vowels) or all(vowel in 'ie' for vowel in vowels):
-		return 'äöy'
-	return 'aou'
+	if not vowels:
+		return f'inconclusive: "{word}" does not contain vowels'
+	elif all(vowel in 'aou' for vowel in vowels): # Rule 1
+		return 'front'
+	elif all(vowel in 'aouie' for vowel in vowels): # Rule 2
+		return 'front + neutral'
+	elif all(vowel in 'äöy' for vowel in vowels): # Rule 3
+		return 'back'
+	elif all(vowel in 'äöyie' for vowel in vowels): # Rule 4
+		return 'back + neutral'
+	else: # Rule 5
+		return 'neutral'
 
 
 def give_example_endings(word: str) -> str:
-	"""Provide an example of an ending for ```word```, given its vowel harmony group."""
-	if return_vowel_group(word) == 'aou':
+	"""Provide example endings for ```word```, given its vowel harmony group."""
+	if return_vowel_group(word) in ['neutral', 'back', 'back + neutral']:
+		return '-ssä, -vät, -kö'
+	elif return_vowel_group(word) in ['front', 'front + neutral']:
 		return '-ssa, -vat, -ko'
-	return '-ssä, -vät, -kö'
+	else:
+		return 'unknown'
 
 
 if __name__ == '__main__':
