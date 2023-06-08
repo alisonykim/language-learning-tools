828135da82abb13f7255bfaa54b310c41fb1a5cf
alisonykim <alison.y.kim@berkeley.edu>
2023-06-08 13:14:37 +0200

    Add example to docstring, improve print statements

--- fi/astevaihtelu.py
+++ fi/astevaihtelu.py
@@ -33,7 +33,24 @@ import utils
 
 
 def get_transformation(word: str) -> Union[Tuple[str, str, str], None, str]:
-	"""Perform consonant gradation from strong to weak forms."""
+	"""
+	Perform consonant gradation from strong to weak forms.
+	
+	Returns:
+		If ```word``` has a final syllable whose consonant(s) undergo grandation, then a tuple of the strong form, weak form, and transformed final syllable is returned. If ```word``` does not undergo consonant gradation, then an empty string is returned.
+	
+	Examples:
+	>>> word1 = 'Afrikka'
+	>>> get_transformation(word1)
+	('kk', 'k', 'ka')
+	>>> word2 = 'Hollanti'
+	>>> get_transformation(word2)
+	('nt', 'nn', 'nni')
+	>>> word3 = 'Suomi'
+	>>> get_transformation(word3)
+	The word "Suomi" either does not undergo consonant gradation or the correct gradation is not currently recognized by this script.
+	''
+	"""
 	final_syllable = utils.get_final_syllable(word)
 	try:
 		for strong_form in constants.STRONG_TO_WEAK_GRAD.keys():
@@ -41,9 +58,8 @@ def get_transformation(word: str) -> Union[Tuple[str, str, str], None, str]:
 				weak_form = constants.STRONG_TO_WEAK_GRAD[strong_form]
 				transformation = re.sub(strong_form, weak_form, final_syllable, flags=re.IGNORECASE)
 				return strong_form, weak_form, transformation
-		return None
 	except KeyError:
-		print(f'The word "{word}" does not undergo consonant gradation.')
+		print(f'The word "{word}" either does not undergo consonant gradation or the correct gradation is not currently recognized by this script.')
 		return ''
 
 
@@ -53,9 +69,8 @@ def produce_nom_plural_example(word: str) -> Union[str, None]:
 	try:	
 		transformation = get_transformation(word)[2]
 		return preceding_syllables + transformation + 't'
-	except IndexError:
-		print(f'The word "{word}" does not undergo consonant gradation.')
-		return None
+	except:
+		return word + 't'
 
 
 if __name__ == '__main__':
@@ -65,34 +80,55 @@ if __name__ == '__main__':
 	forms = get_transformation(word)
 	example = produce_nom_plural_example(word)
 
-	if forms is not None and len(forms) == 3:
-		print('')
-		strong, weak, transformation = forms
-		if not guess_weak:
-			weak = ''
-		if strong == guess_strong and weak == guess_weak:
+	print('')
+
+	if example == word + 't':
+		if not guess_strong and not guess_weak:
 			print('Correct!')
-			print(f'The word "{word}" undergoes the following consonant gradation: "{strong}" -> "{weak}"')
-			print(f'The nominative plural form of "{word}" is "{example}".')
-		elif strong == guess_strong and weak != guess_weak:
-			print(f'Half correct!')
-			print(f'You guessed the correct STRONG form "{guess_strong}".')
-			print(f'However, you guessed the wrong WEAK form: it is not "{guess_weak}", but rather "{weak}".')
 			print('')
-			print(f'The word "{word}" undergoes the following consonant gradation: "{strong}" -> "{weak}".')
-			print(f'The nominative plural form of "{word}" is "{example}".')
-		elif strong != guess_strong and weak == guess_weak:
-			print(f'Half correct!')
-			print(f'You guessed the correct WEAK form "{guess_weak}".')
-			print(f'However, you guessed the wrong STRONG form: it is not "{guess_strong}", but rather "{strong}".')
-			print('')
-			print(f'The word "{word}" undergoes the following consonant gradation: "{strong}" -> "{weak}".')
+			print(f'The word "{word}" does not undergo consonant gradation.')
 			print(f'The nominative plural form of "{word}" is "{example}".')
 		else:
-			print(f'Incorrect!')
-			print(f'You guessed the STRONG form "{guess_strong}" and WEAK form "{guess_weak}".')
+			print('Incorrect!')
 			print('')
-			print(f'The word "{word}" undergoes the following consonant gradation: "{strong}" -> "{weak}".')
+			print(f'The word "{word}" does not undergo consonant gradation.')
 			print(f'The nominative plural form of "{word}" is "{example}".')
 	else:
-		print(f'The word {word} does not undergo consonant gradation.')
+		if forms is not None and len(forms) == 3:
+			strong, weak, transformation = forms
+			if not guess_weak:
+				weak = ''
+			
+			if strong == guess_strong and weak == guess_weak:
+				print('Correct!')
+				print('')
+				if example == word + 't':
+					print(f'The word "{word}" does not undergo consonant gradation.')
+				else:
+					print(f'The word "{word}" undergoes the following consonant gradation: "{strong}" -> "{weak}"')
+					print(f'The nominative plural form of "{word}" is "{example}".')				
+			elif strong == guess_strong and weak != guess_weak:
+				print(f'Half correct!')
+				print('')
+				print(f'You guessed the correct STRONG form "{guess_strong}".')
+				print(f'However, you guessed the wrong WEAK form: it is not "{guess_weak}", but rather "{weak}".')
+				print('')
+				print(f'The word "{word}" undergoes the following consonant gradation: "{strong}" -> "{weak}".')
+				print(f'The nominative plural form of "{word}" is "{example}".')
+			elif strong != guess_strong and weak == guess_weak:
+				print(f'Half correct!')
+				print('')
+				print(f'You guessed the correct WEAK form "{guess_weak}".')
+				print(f'However, you guessed the wrong STRONG form: it is not "{guess_strong}", but rather "{strong}".')
+				print('')
+				print(f'The word "{word}" undergoes the following consonant gradation: "{strong}" -> "{weak}".')
+				print(f'The nominative plural form of "{word}" is "{example}".')
+			else:
+				print(f'Incorrect!')
+				print('')
+				print(f'You guessed the STRONG form "{guess_strong}" and WEAK form "{guess_weak}".')
+				print('')
+				print(f'The word "{word}" undergoes the following consonant gradation: "{strong}" -> "{weak}".')
+				print(f'The nominative plural form of "{word}" is "{example}".')
+		else:
+			print(f'The word "{word}" either does not undergo consonant gradation or its gradation is not recognized by the script at this time.')
